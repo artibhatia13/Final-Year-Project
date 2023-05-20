45ce5eb52f4bdf1f5a42cf997ab183cd1e48dd7c
Khushi Shah <khushi.s@compile.com>
2023-05-20 14:02:52 +0530

    preview input gemstone image

--- frontend/src/components/Classify.js
+++ frontend/src/components/Classify.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Text, Flex, Box, Button } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
 import axios from "axios";
 
 class Classify extends React.Component {
@@ -6,6 +8,7 @@ class Classify extends React.Component {
     super(props);
     this.state = {
       prediction: null,
+      imagePreview: null,
     };
     this.uploadImage = this.uploadImage.bind(this);
   }
@@ -14,6 +17,12 @@ class Classify extends React.Component {
     const file = event.target.files[0];
     const formData = new FormData();
     formData.append("file", file);
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      this.setState({ imagePreview: reader.result });
+    };
+    reader.readAsDataURL(file);
 
     axios
       .post("http://localhost:5000/predict", formData, {
@@ -30,13 +39,32 @@ class Classify extends React.Component {
   }
 
   render() {
-    const { prediction } = this.state;
+    const { prediction, imagePreview } = this.state;
 
     return (
-      <div>
-        <input type="file" onChange={this.uploadImage} />
-        {prediction && <p>Prediction: {prediction}</p>}
-      </div>
+      <Box padding={20}>
+        <Text fontSize="xl">
+          Capture the beauty of your gemstone and let our advanced machine
+          learning algorithm unravel its true identity. Upload an image of your
+          gemstone below, and our powerful classification system will harness
+          the power of artificial intelligence to determine its precise gemstone
+          class.
+        </Text>
+        <Box padding={10}>
+          <input type="file" onChange={this.uploadImage} />
+          {imagePreview && (
+            <div>
+              <h3>Uploaded Image:</h3>
+              <img
+                src={imagePreview}
+                alt="Uploaded"
+                style={{ maxWidth: "100%" }}
+              />
+            </div>
+          )}
+          {prediction && <p>Prediction: {prediction}</p>}
+        </Box>
+      </Box>
     );
   }
 }
